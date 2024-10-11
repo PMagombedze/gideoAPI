5f887ed59bb82779382b2e864a4b7a77393a482b
PMagombedze <percymagoras@outlook.com>
2024-10-11 20:36:25 +0200

    Add home route to views.py and imports.html template

--- views.py
+++ views.py
@@ -5,3 +5,8 @@ products = Blueprint('products', __name__)
 @products.route('/products')
 def dash():
     return render_template('products.html')
+
+
+@products.route('/')
+def home():
+    return render_template('index.html')
